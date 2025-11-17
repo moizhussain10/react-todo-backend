d028bca4e63d1d9e27f53059275a27e9523b2eb1
MrAsh-007 <mrash2006@gmail.com>
2025-11-17 09:42:35 +0500

    .

--- server.js
+++ server.js
@@ -6,10 +6,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const app = express();
-app.use(cors({
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"]
-}));
+app.use(cors());
 app.use(express.json());
 
 // ✅ MongoDB connection
