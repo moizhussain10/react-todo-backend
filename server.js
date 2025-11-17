3e9fd20f5ca473ba8c3dc497466086e7f8050a8c
moizhussain10 <moizhussain014@gmail.com>
2025-11-17 10:20:13 +0500

    Remove dotenv import and configuration
    Remove dotenv configuration from server.js

--- server.js
+++ server.js
@@ -1,9 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const app = express();
 app.use(cors());
